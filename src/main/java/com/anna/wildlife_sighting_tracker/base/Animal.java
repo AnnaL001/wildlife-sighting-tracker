80cec379b2f6ea37fb0e751372d540df8969f208
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-07 21:08:53 +0300

    Add method for comparison of objects

--- src/main/java/com/anna/wildlife_sighting_tracker/base/Animal.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/base/Animal.java
@@ -1,5 +1,7 @@
 package com.anna.wildlife_sighting_tracker.base;
 
+import java.util.Objects;
+
 public abstract class Animal {
   protected int id;
   protected String imageUrl;
@@ -25,5 +27,18 @@ public abstract class Animal {
 
   public String getCategory() {
     return category;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Animal animal = (Animal) o;
+    return id == animal.id && speciesId == animal.speciesId && Objects.equals(imageUrl, animal.imageUrl) && Objects.equals(name, animal.name) && Objects.equals(category, animal.category);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, imageUrl, name, speciesId, category);
   }
 }
