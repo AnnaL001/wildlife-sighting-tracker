4ac62d5ce7b1d37ed048c596a441aac879236098
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-08 19:34:54 +0300

    Add implementations of database read functionality of species

---
+++ src/main/java/com/anna/wildlife_sighting_tracker/dao/Sql2oSpeciesDao.java
@@ -0,0 +1,40 @@
+package com.anna.wildlife_sighting_tracker.dao;
+
+import com.anna.wildlife_sighting_tracker.interfaces.ImmutableDatabaseDao;
+import com.anna.wildlife_sighting_tracker.models.Species;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oSpeciesDao implements ImmutableDatabaseDao<Species> {
+  private final Sql2o sql2o;
+
+  public Sql2oSpeciesDao(Sql2o sql2o) {
+    this.sql2o = sql2o;
+  }
+
+  @Override
+  public List<Species> getAll() {
+    try(Connection connection = sql2o.open()){
+      return connection.createQuery("SELECT * FROM species")
+              .executeAndFetch(Species.class);
+    } catch (Sql2oException exception){
+      exception.printStackTrace();
+      return null;
+    }
+  }
+
+  @Override
+  public Species get(int id) {
+    try(Connection connection = sql2o.open()){
+      return connection.createQuery("SELECT * FROM species WHERE id=:id")
+              .addParameter("id", id)
+              .executeAndFetchFirst(Species.class);
+    } catch (Sql2oException exception){
+      exception.printStackTrace();
+      return null;
+    }
+  }
+}
