5d284523ebf764e5c6524affd327ea0ac7557836
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-08 10:58:12 +0300

    Remove unused code

--- src/main/java/com/anna/wildlife_sighting_tracker/base/WildlifeTrackerDB.java
+++
@@ -1,7 +0,0 @@
-package com.anna.wildlife_sighting_tracker.base;
-
-import org.sql2o.Sql2o;
-
-public class WildlifeTrackerDB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "anna", "SurMaRoute01$");
-}
