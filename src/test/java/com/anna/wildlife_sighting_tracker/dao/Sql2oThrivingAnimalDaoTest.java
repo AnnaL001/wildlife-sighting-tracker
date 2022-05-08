7c26d31eac719659e6bd70b3b4cbdeb43f38a9f8
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-08 13:12:32 +0300

    Add tests for database update and delete functionalities

--- src/test/java/com/anna/wildlife_sighting_tracker/dao/Sql2oThrivingAnimalDaoTest.java
+++ src/test/java/com/anna/wildlife_sighting_tracker/dao/Sql2oThrivingAnimalDaoTest.java
@@ -1,6 +1,5 @@
 package com.anna.wildlife_sighting_tracker.dao;
 
-import com.anna.wildlife_sighting_tracker.models.EndangeredAnimal;
 import com.anna.wildlife_sighting_tracker.models.ThrivingAnimal;
 import com.anna.wildlife_sighting_tracker.parameter_resolver.ThrivingAnimalParameterResolver;
 import org.junit.jupiter.api.*;
@@ -24,14 +23,14 @@ class Sql2oThrivingAnimalDaoTest {
 
   @Test
   @DisplayName("Test that a thriving animal can be added to the database")
-  public void add_addsEndangeredAnimal_true(ThrivingAnimal thrivingAnimal) {
+  public void add_addsThrivingAnimal_true(ThrivingAnimal thrivingAnimal) {
     animalDao.add(thrivingAnimal);
     assertTrue(animalDao.getAll().contains(thrivingAnimal));
   }
 
   @Test
   @DisplayName("Test that id is set when a thriving animal is added to the database")
-  public void add_addsEndangeredAnimalSetsId_true(ThrivingAnimal thrivingAnimal) {
+  public void add_addsThrivingAnimalAndSetsId_true(ThrivingAnimal thrivingAnimal) {
     int initialId = thrivingAnimal.getId();
     animalDao.add(thrivingAnimal);
     assertNotEquals(initialId, thrivingAnimal.getId());
@@ -39,7 +38,7 @@ class Sql2oThrivingAnimalDaoTest {
 
   @Test
   @DisplayName("Test that all added thriving animals can be retrieved from the database")
-  public void getAll_returnsAddedEndangeredAnimals_true(ThrivingAnimal thrivingAnimal) {
+  public void getAll_returnsAddedThrivingAnimals_true(ThrivingAnimal thrivingAnimal) {
     animalDao.add(thrivingAnimal);
     assertEquals(1, animalDao.getAll().size());
   }
@@ -52,12 +51,49 @@ class Sql2oThrivingAnimalDaoTest {
 
   @Test
   @DisplayName("Test that a thriving animal can be retrieved from the database")
-  public void get_returnsEndangeredAnimalWithSameId_true(ThrivingAnimal thrivingAnimal) {
+  public void get_returnsThrivingAnimalWithSameId_true(ThrivingAnimal thrivingAnimal) {
     animalDao.add(thrivingAnimal);
     assertEquals(thrivingAnimal, animalDao.get(thrivingAnimal.getId()));
   }
 
+  @Test
+  @DisplayName("Test that a thriving animal's data can be updated")
+  public void update_updatesDataOfThrivingAnimal_true(ThrivingAnimal thrivingAnimal) {
+    animalDao.add(thrivingAnimal);
+    thrivingAnimal.setImage("https://wildlife_tracker/lion.jpg");
+    thrivingAnimal.setName("Cecil");
+    thrivingAnimal.setSpeciesId(2);
+    animalDao.update(thrivingAnimal);
+    ThrivingAnimal foundAnimal = animalDao.get(thrivingAnimal.getId());
+    assertEquals("https://wildlife_tracker/lion.jpg", foundAnimal.getImage());
+    assertEquals("Cecil", foundAnimal.getName());
+    assertEquals(2, foundAnimal.getSpeciesId());
+  }
 
+  @Test
+  @DisplayName("Test that a thriving animal's data can be deleted")
+  public void delete_deletesAThrivingAnimal_true(ThrivingAnimal thrivingAnimal) {
+    animalDao.add(thrivingAnimal);
+    animalDao.delete(thrivingAnimal.getId());
+    assertFalse(animalDao.getAll().contains(thrivingAnimal));
+  }
+
+  @Test
+  @DisplayName("Test that all thriving animals' data can be deleted")
+  public void deleteAll_deletesAllThrivingAnimals_true(ThrivingAnimal thrivingAnimal) {
+    animalDao.add(thrivingAnimal);
+    animalDao.add(setUpThrivingAnimal());
+    animalDao.deleteAll();
+    assertEquals(0, animalDao.getAll().size());
+  }
+
+  private ThrivingAnimal setUpThrivingAnimal(){
+    return new ThrivingAnimal(
+            "https://upload.wikimedia.org/wikipedia/commons/1/1e/Cecil_the_lion_at_Hwange_National_Park_%284516560206%29.jpg",
+            "Poppy",
+            1
+    );
+  }
 
   @AfterEach
   void tearDown() {
