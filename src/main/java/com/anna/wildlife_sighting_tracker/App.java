4861087c74fc0d15d26778f0581ee12b2cf217fd
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-09 03:27:46 +0300

    Add route to home page

--- src/main/java/com/anna/wildlife_sighting_tracker/App.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/App.java
@@ -1,7 +1,16 @@
 package com.anna.wildlife_sighting_tracker;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
-
+    staticFileLocation("public/");
+    get("/", (request, response) -> new HandlebarsTemplateEngine().render(
+            new ModelAndView(new HashMap<>(), "index.hbs")
+    ));
   }
 }
