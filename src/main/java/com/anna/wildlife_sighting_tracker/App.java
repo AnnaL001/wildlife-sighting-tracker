bb15d3065d11dfbe7b827f1ecc287df16483a1ef
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-09 11:17:28 +0300

    Add routes to animal list page and animal profile page

--- src/main/java/com/anna/wildlife_sighting_tracker/App.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/App.java
@@ -1,6 +1,8 @@
 package com.anna.wildlife_sighting_tracker;
 
 import com.anna.wildlife_sighting_tracker.dao.*;
+import com.anna.wildlife_sighting_tracker.models.EndangeredAnimal;
+import com.anna.wildlife_sighting_tracker.models.ThrivingAnimal;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -8,6 +10,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.Integer.parseInt;
 import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
@@ -31,6 +34,30 @@ public class App {
       model.put("species_count", speciesDao.getAll().size());
       return new HandlebarsTemplateEngine().render(
               new ModelAndView(model, "home.hbs")
+      );
+    });
+
+    get("/animals", (request, response) -> {
+      model.put("endangered", endangeredAnimalDao.getAll());
+      model.put("thriving", thrivingAnimalDao.getAll());
+      return new HandlebarsTemplateEngine().render(
+              new ModelAndView(model, "animals.hbs")
+      );
+    });
+
+    get("/animals/:id", (request, response) -> {
+      ThrivingAnimal thrivingAnimal = thrivingAnimalDao.get(parseInt(request.params("id")));
+      EndangeredAnimal endangeredAnimal = endangeredAnimalDao.get(parseInt(request.params("id")));
+      model.put("thriving", thrivingAnimal);
+      model.put("endangered", endangeredAnimal);
+      if(thrivingAnimal != null){
+        model.put("species", speciesDao.get(thrivingAnimal.getSpeciesId()));
+      }
+      if(endangeredAnimal != null){
+        model.put("species", speciesDao.get(endangeredAnimal.getSpeciesId()));
+      }
+      return new HandlebarsTemplateEngine().render(
+              new ModelAndView(model, "animal.hbs")
       );
     });
   }
