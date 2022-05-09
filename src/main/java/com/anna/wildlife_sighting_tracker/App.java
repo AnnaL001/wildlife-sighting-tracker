9ef9e23c248fedb38c72cad530dfd6545d7aaf49
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-09 12:18:32 +0300

    Optimize imports

--- src/main/java/com/anna/wildlife_sighting_tracker/App.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/App.java
@@ -11,7 +11,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.lang.Integer.parseInt;
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("public/");
