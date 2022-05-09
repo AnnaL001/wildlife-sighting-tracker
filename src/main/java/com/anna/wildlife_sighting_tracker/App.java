c47cbffa99961fe5013b8a540ab1125a0f4f0809
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-09 04:08:13 +0300

    Add route for home page

--- src/main/java/com/anna/wildlife_sighting_tracker/App.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/App.java
@@ -1,16 +1,37 @@
 package com.anna.wildlife_sighting_tracker;
 
+import com.anna.wildlife_sighting_tracker.dao.*;
+import org.sql2o.Sql2o;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     staticFileLocation("public/");
+    Map<String, Object> model = new HashMap<>();
+    Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "anna", "SurMaRoute01$");
+    Sql2oEndangeredAnimalDao endangeredAnimalDao = new Sql2oEndangeredAnimalDao(sql2o);
+    Sql2oThrivingAnimalDao thrivingAnimalDao = new Sql2oThrivingAnimalDao(sql2o);
+    Sql2oSightingDao sightingDao = new Sql2oSightingDao(sql2o);
+    Sql2oRangerDao rangerDao = new Sql2oRangerDao(sql2o);
+    Sql2oSpeciesDao speciesDao = new Sql2oSpeciesDao(sql2o);
+
     get("/", (request, response) -> new HandlebarsTemplateEngine().render(
             new ModelAndView(new HashMap<>(), "index.hbs")
     ));
+
+    get("/home", (request, response) -> {
+      model.put("animals_count", endangeredAnimalDao.getAll().size() + thrivingAnimalDao.getAll().size());
+      model.put("sightings_count", sightingDao.getAll().size());
+      model.put("rangers_count", rangerDao.getAll().size());
+      model.put("species_count", speciesDao.getAll().size());
+      return new HandlebarsTemplateEngine().render(
+              new ModelAndView(model, "home.hbs")
+      );
+    });
   }
 }
