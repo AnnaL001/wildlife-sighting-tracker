e0e158ce898b12d6b4962f0a927364e35e363839
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-10 03:06:45 +0300

    Update model class for Ranger data

--- src/main/java/com/anna/wildlife_sighting_tracker/models/Ranger.java
+++ src/main/java/com/anna/wildlife_sighting_tracker/models/Ranger.java
@@ -1,11 +1,16 @@
 package com.anna.wildlife_sighting_tracker.models;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Ranger {
   private int id;
   private final String name;
   private final String badge;
   private final int phone;
   private final String email;
+
+  private List<Sighting> sightings;
 
   public Ranger(String name, String badge, int phone, String email) {
     this.name = name;
@@ -34,5 +39,24 @@ public class Ranger {
     return email;
   }
 
+  public List<Sighting> getSightings() {
+    return sightings;
+  }
 
+  public void setSightings(List<Sighting> sightings) {
+    this.sightings = sightings;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Ranger ranger = (Ranger) o;
+    return phone == ranger.phone && Objects.equals(name, ranger.name) && Objects.equals(badge, ranger.badge) && Objects.equals(email, ranger.email) && Objects.equals(sightings, ranger.sightings);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, badge, phone, email, sightings);
+  }
 }
